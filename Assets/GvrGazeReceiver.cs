7d01323efb3ffa8f8965ef18244743d2c81ff3fc
Rudolf <rudolfcicko2016@gmail.com>
2016-10-21 10:38:28 +0100

    Added pointing to giant cube

---
+++ Assets/GvrGazeReceiver.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class GvrGazeReceiver : MonoBehaviour, IGvrGazeResponder {
+    public float speed = 1;
+    private bool canMove;
+    public void OnGazeEnter()
+    {
+        print("Gaze Enter");
+        canMove = true;
+    }
+
+    public void OnGazeExit()
+    {
+        print("Gaze Exit");
+        canMove = false;
+    }
+
+    public void OnGazeTrigger()
+    {
+        throw new NotImplementedException();
+    }
+    
+    public void FixedUpdate ()
+    {
+        if (canMove)
+            GetComponent<Rigidbody>().transform.Translate(Vector3.up * speed);
+        
+    }
+
+    public void maximumForce()
+    {
+        GetComponent<Rigidbody>().AddForce(Vector3.up * speed * 100);
+    }
+
+
+}
