261644ece98ee573e3664e7663f1814a6c03f46d
Rudolf <rudolfcicko2016@gmail.com>
2016-10-27 18:20:12 +0100

    Added Reticle

--- Assets/GunManager.cs
+++ Assets/GunManager.cs
@@ -12,7 +12,7 @@ public class GunManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
         {
             GameObject bullet = (GameObject)Instantiate(bulletPrefab, transform.position, Quaternion.identity);
             bullet.GetComponent<Rigidbody>().AddForce(transform.forward * bulletSpeed, ForceMode.Impulse);
